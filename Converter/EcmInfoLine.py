74c01978dc333384ae4fec3efb48c0d000e79023
martina-3 <83170284+martina-3@users.noreply.github.com>
2022-05-02 17:08:23 +0300

    Add files via upload

--- Converter/EcmInfoLine.py
+++ Converter/EcmInfoLine.py
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-#2022
+# 2022 add internet broadcast
 
 from enigma import iServiceInformation
 from Components.Converter.Converter import Converter
